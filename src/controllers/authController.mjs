b8bf535a7e31096827eed2f28adc8a1ae3b817c7
SaideLeon <saideomarsaid@gmail.com>
2024-12-21 23:48:33 +0200

    local

--- src/controllers/authController.mjs
+++ src/controllers/authController.mjs
@@ -1,7 +1,7 @@
 // controllers/authController.mjs
 import { PrismaClient } from '@prisma/client';
 import AppError from '../utils/AppError.mjs';
-import { generateToken } from '../utils/generateToken.mjs';
+import { generateToken } from '../utils/generateToken.js';
 import { comparePasswords } from '../utils/hashPassword.mjs';
 
 const prisma = new PrismaClient();
